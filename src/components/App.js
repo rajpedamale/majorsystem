5a8dd24f0043e27dc71a64726e65cef7bf46c9d0
Rajesh Pedamale <pedamale@gmail.com>
2019-03-24 11:46:55 +1100

    Refactor to Responsive layout

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import React from "react";
 import { hot } from "react-hot-loader";
 import PropTypes from "prop-types";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "reactstrap";
 
 import HomePage from "./HomePage";
 import NotFoundPage from "./NotFoundPage";
@@ -16,19 +17,19 @@ class App extends React.Component {
     return (
       <div>
         <NavBar />
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col" />
-            <div className="col-sm-6 ">
+        <Container fluid={true}>
+          <Row>
+            <Col md="" />
+            <Col sm="12" md="8">
               <Switch>
                 <Route exact path="/" component={HomePage} />
                 <Route path="/randomnumbers" component={RandomNumbers} />
                 <Route component={NotFoundPage} />
               </Switch>
-            </div>
-            <div className="col" />
-          </div>
-        </div>
+            </Col>
+            <Col md="" />
+          </Row>
+        </Container>
       </div>
     );
   }
